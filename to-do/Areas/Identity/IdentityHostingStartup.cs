1e152f720a6c81ddba0be2835e38fa158645ad4b
Konrad Opalinski <opalkonrad@gmail.com>
2020-03-14 22:28:13 +0100

    Add Identity and simple homepage

---
+++ to-do/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using to_do.Data;
+
+[assembly: HostingStartup(typeof(to_do.Areas.Identity.IdentityHostingStartup))]
+namespace to_do.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
