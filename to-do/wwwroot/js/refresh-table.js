64b8e8851f39515a662916d03dc1a4bc5937f74a
Konrad Opalinski <opalkonrad@gmail.com>
2020-03-23 21:29:23 +0100

    Fix warning with synchronous XMLHttpRequest

--- to-do/wwwroot/js/refresh-table.js
+++ to-do/wwwroot/js/refresh-table.js
@@ -1,4 +1,12 @@
 ﻿$(document).ready(function () {
+    /* To prevent warning:
+     * [Deprecation] Synchronous XMLHttpRequest on the main thread is deprecated because of its detrimental
+     * effects to the end user's experience. For more help, check https://xhr.spec.whatwg.org/.
+     */
+    $.ajaxPrefilter(function (options, original_Options, jqXHR) {
+        options.async = true;
+    });
+
     $.ajax({
         url: '/ToDoes/RefreshToDoTable',
         success: function (result) {
